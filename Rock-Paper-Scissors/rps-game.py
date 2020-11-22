162f3fd93f8c8c13fee138a4710d869f086ee491
itsalidag <61254104+itsalidag@users.noreply.github.com>
2020-11-22 23:14:20 +0300

    Update rps-game.py
    now asks for user name.

--- Rock-Paper-Scissors/rps-game.py
+++ Rock-Paper-Scissors/rps-game.py
@@ -6,23 +6,25 @@ secenek = ["Taş", "Kağıt", "Makas"]
 kullanıcıskor = 0
 robotskor = 0
 a = 0
+oyuncuadi = str(input("Oyuncu Adı: "))
 while a < 10:
     secim = random.randint(0,2)
     robotsecim = secenek[secim]
     kullanıcı = str(input("Taş için (T), Kağıt için (K), Makas için (M) kullan ----> "))
     if kullanıcı in ["T","t"]:
         if robotsecim == secenek[0]:
+            print("Aynıyız, sayılmaz.")
             continue
         elif robotsecim == secenek[1]:
             robotskor = robotskor+1
             print("Robot +1")
         elif robotsecim == secenek[2]:
             kullanıcıskor = kullanıcıskor+1
-            print("İnsan +1")
+            print("{} +1".format(oyuncuadi))
     if kullanıcı in ["k","K"]:
         if robotsecim == secenek[0]:
             kullanıcıskor = kullanıcıskor +1
-            print("İnsan +1")
+            print("{} +1".format(oyuncuadi))
         elif robotsecim == secenek[1]:
             print("Aynıyız, sayılmaz")
             continue   
@@ -35,11 +37,12 @@ while a < 10:
             print("Robot +1")
         elif robotsecim == secenek[1]:
             kullanıcıskor = kullanıcıskor+1
-            print("İnsan +1")
+            print("{} +1".format(oyuncuadi))
         elif robotsecim == secenek[2]:
             print("Aynıyız, sayılmaz")
             continue          
     print("Tur: {}".format(a))
     a = a+1
 
-print("Oyun sonu! Kazanan {}, skor = robot : {} İnsan: {}".format(("robot" if robotskor>kullanıcıskor else "İnsan"), robotskor,kullanıcıskor))
+print("Oyun sonu! Kazanan {}, skor = robot : {} {}: {}".format(("robot" if robotskor>kullanıcıskor else oyuncuadi), robotskor,oyuncuadi,kullanıcıskor))
+
