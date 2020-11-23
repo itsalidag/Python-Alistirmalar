390d5581f2fff6fdf3204afb81f1c1a69eca01cd
itsalidag <61254104+itsalidag@users.noreply.github.com>
2020-11-24 00:33:35 +0300

    Create source-kode-pdf-merge.py

---
+++ Pdf-merge/source-kode-pdf-merge.py
@@ -0,0 +1,12 @@
+from PyPDF2 import PdfFileMerger
+import os
+merger = PdfFileMerger()
+for items in os.listdir():
+    if items.endswith(".pdf"):
+        merger.append(items)
+merger.write("Final_pdf.pdf")
+merger = PdfFileMerger()
+with open(originalFile, "rb") as fin:
+    merger.append(PdfFileMerger(fin))   
+os.remove(originalFile)
+merger.close()
